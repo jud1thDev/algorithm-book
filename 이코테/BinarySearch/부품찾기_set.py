e4ba8b4a58487d77ceaa1a9599585f764a51e8a5
judith <8w8u8@naver.com>
2025-08-04 15:39:15 +0900

    Upload 부품찾기_CountingSort.py

--- 이코테/BinarySearch/부품찾기_set.py
+++ 이코테/BinarySearch/부품찾기_set.py
@@ -1,5 +1,5 @@
 n = int(input())
-array = set(map(int, input().split())) # set 집합은 중복
+array = set(map(int, input().split())) # set 집합은 중복 허용하지 않으므로, 트겆ㅇ 데이터가 존재하는지 검사할 때 효과적
 m = int(input())
 x = list(map(int, input().split()))
 
